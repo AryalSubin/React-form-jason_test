79d7c3b69b3950bcd1c28b38252dc8e870219306
AryalSubin <aryalsuubin@gmail.com>
2022-11-02 23:18:39 +0545

    third try

--- src/App.js
+++ src/App.js
@@ -2,29 +2,12 @@ import "./App.css";
 import Form from "./components/Form";
 
 function App() {
-  const schema = {
-    type: "object",
-    properties: {
-      employees: {
-        type: "array",
-        items: [
-          {
-            type: "object",
-            properties: {
-              name: {
-                type: "string",
-              },
-              email: {
-                type: "email",
-              },
-            },
-            required: ["name", "email"],
-          },
-        ],
-      },
-    },
-    required: ["employees"],
-  };
+  const schema = [
+    { tag: "div", text: "first div" },
+    { tag: "div", text: "second div" },
+    { tag: "p", text: "a paragraph" },
+    { tag: "h1", text: "heading 1" },
+  ];
   return (
     <div className="App">
       <Form data={schema} />
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,18 +1,21 @@
 import React from "react";
 
 const Form = (schema) => {
-  const data = schema.data.properties.employees.items;
-  console.log(data);
+  // const data = schema.data.properties.employees.items;
+  console.log(schema);
   return (
     <>
       <form>
-        {data.map((item, id) => {
-          return (
-            <div key={id}>
-              <input type={item.properties.email.type} />
-              <input type={item.properties.name.type} />
-            </div>
-          );
+        {schema.data.map((item) => {
+          if (item.tag === "div") {
+            return <div>{item.text}</div>;
+          } else if (item.tag === "p") {
+            return <p>{item.text}</p>;
+          } else if (item.tag === "h1") {
+            return <h1>{item.text}</h1>;
+          } else {
+            return <>loading...</>;
+          }
         })}
         <input type="submit" />
       </form>
